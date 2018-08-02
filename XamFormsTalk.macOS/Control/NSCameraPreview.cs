4a10d096f4b1b8c9495e3def8fcef656fe3b5caf
John VanSickle <jwvansickle@icloud.com>
2018-08-02 19:22:23 -0400

    Add mac os camera preview renderer

---
+++ XamFormsTalk.macOS/Control/NSCameraPreview.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using AppKit;
+using AVFoundation;
+using CoreGraphics;
+using Foundation;
+using XamFormsTalk.Contracts;
+
+namespace XamFormsTalk.macOS.Control
+{
+    public class NSCameraPreview : NSView
+    {
+        AVCaptureVideoPreviewLayer previewLayer;
+
+        public event EventHandler<EventArgs> Tapped;
+
+        public AVCaptureSession CaptureSession { get; private set; }
+
+        public bool IsPreviewing { get; set; }
+
+        public NSCameraPreview()
+        {
+            IsPreviewing = false;
+            Initialize();
+        }
+
+        public override void DrawRect(CGRect dirtyRect)
+        {
+            base.DrawRect(dirtyRect);
+            previewLayer.Frame = dirtyRect;
+        }
+
+        public override void MouseDown(NSEvent theEvent)
+        {
+            base.MouseDown(theEvent);
+            OnTapped();
+        }
+
+        protected virtual void OnTapped()
+        {
+            Tapped?.Invoke(this, new EventArgs());
+        }
+
+        void Initialize()
+        {
+            CaptureSession = new AVCaptureSession();
+            previewLayer = new AVCaptureVideoPreviewLayer(CaptureSession)
+            {
+                Frame = Bounds,
+                VideoGravity = AVLayerVideoGravity.ResizeAspectFill
+            };
+
+            var videoDevices = AVCaptureDevice.DevicesWithMediaType(AVMediaType.Video);
+            var device = videoDevices.FirstOrDefault();
+
+            if (device == null)
+            {
+                return;
+            }
+
+            var input = new AVCaptureDeviceInput(device, out NSError error);
+            CaptureSession.AddInput(input);
+            WantsLayer = true;
+            Layer.AddSublayer(previewLayer);
+            CaptureSession.StartRunning();
+            IsPreviewing = true;
+        }
+    }
+}
---
+++ XamFormsTalk.macOS/Renderer/CameraPreviewRenderer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+using XamFormsTalk.Element;
+using XamFormsTalk.macOS.Control;
+using XamFormsTalk.macOS.Renderer;
+
+[assembly: ExportRenderer(typeof(CameraPreview), typeof(CameraPreviewRenderer))]
+namespace XamFormsTalk.macOS.Renderer
+{
+    public class CameraPreviewRenderer : ViewRenderer<CameraPreview, NSCameraPreview>
+    {
+        NSCameraPreview nsCameraPreview;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<CameraPreview> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                nsCameraPreview = new NSCameraPreview();
+                SetNativeControl(nsCameraPreview);
+            }
+            if (e.OldElement != null)
+            {
+                // Unsubscribe
+                nsCameraPreview.Tapped -= OnCameraPreviewTapped;
+            }
+            if (e.NewElement != null)
+            {
+                // Subscribe
+                nsCameraPreview.Tapped += OnCameraPreviewTapped;
+            }
+        }
+
+        void OnCameraPreviewTapped(object sender, EventArgs e)
+        {
+            if (nsCameraPreview.IsPreviewing)
+            {
+                nsCameraPreview.CaptureSession.StopRunning();
+                nsCameraPreview.IsPreviewing = false;
+            }
+            else
+            {
+                nsCameraPreview.CaptureSession.StartRunning();
+                nsCameraPreview.IsPreviewing = true;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Control.CaptureSession.Dispose();
+                Control.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
