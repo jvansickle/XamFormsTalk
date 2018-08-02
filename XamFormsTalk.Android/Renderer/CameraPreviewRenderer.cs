95409de39b433d4bce1589a44bb3ee670852639a
John VanSickle <jwvansickle@icloud.com>
2018-08-02 19:05:35 -0400

    Add camera preview renderer for droid

---
+++ XamFormsTalk.Android/Renderer/CameraPreviewRenderer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Android.Content;
+using Android.Hardware;
+using Xamarin.Forms.Platform.Android;
+using XamFormsTalk.Droid.Control;
+
+namespace XamFormsTalk.Droid.Renderer
+{
+    public class CameraPreviewRenderer : ViewRenderer<Element.CameraPreview, CameraPreview>
+    {
+        CameraPreview cameraPreview;
+
+        public CameraPreviewRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Element.CameraPreview> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                cameraPreview = new CameraPreview(Context);
+                SetNativeControl(cameraPreview);
+            }
+
+            if (e.OldElement != null)
+            {
+                // Unsubscribe
+                cameraPreview.Click -= OnCameraPreviewClicked;
+            }
+            if (e.NewElement != null)
+            {
+                Control.Preview = Camera.Open((int)e.NewElement.Camera);
+
+                // Subscribe
+                cameraPreview.Click += OnCameraPreviewClicked;
+            }
+        }
+
+        void OnCameraPreviewClicked(object sender, EventArgs e)
+        {
+            if (cameraPreview.IsPreviewing)
+            {
+                cameraPreview.Preview.StopPreview();
+                cameraPreview.IsPreviewing = false;
+            }
+            else
+            {
+                cameraPreview.Preview.StartPreview();
+                cameraPreview.IsPreviewing = true;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Control.Preview.Release();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
