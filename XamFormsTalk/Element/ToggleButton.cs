81f047ccaa839065b39c55305210a09a3bb75323
John VanSickle <jwvansickle@icloud.com>
2018-08-02 18:17:34 -0400

    Add toggle button to main page
    Toggle button is a Button class that has a custom IsToggled bindable property for managing visual state and allowing a page to have a method fired when the button is toggled

---
+++ XamFormsTalk/Element/ToggleButton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamFormsTalk.Element
+{
+    public class ToggleButton : Button
+    {
+        public event EventHandler<ToggledEventArgs> Toggled;
+
+        public static readonly BindableProperty IsToggledProperty =
+            BindableProperty.Create(nameof(IsToggled),
+                                    typeof(bool),
+                                    typeof(ToggleButton),
+                                    false,
+                                    BindingMode.TwoWay,
+                                    propertyChanged: OnIsToggledChanged);
+
+        public bool IsToggled
+        {
+            get { return (bool)GetValue(IsToggledProperty); }
+            set { SetValue(IsToggledProperty, value); }
+        }
+
+        public ToggleButton()
+        {
+            Clicked += OnClicked;
+        }
+
+        protected override void OnParentSet()
+        {
+            base.OnParentSet();
+            VisualStateManager.GoToState(this, "ToggledOff");
+        }
+
+        void OnClicked(object sender, EventArgs args)
+        {
+            IsToggled ^= true;
+        }
+
+        static void OnIsToggledChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            ToggleButton toggleButton = (ToggleButton)bindable;
+            bool isToggled = (bool)newValue;
+
+            toggleButton.Toggled?.Invoke(toggleButton, new ToggledEventArgs(isToggled));
+
+            VisualStateManager.GoToState(toggleButton, isToggled ? "ToggledOn" : "ToggledOff");
+        }
+    }
+}
---
+++ XamFormsTalk/Page/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace XamFormsTalk.Page
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnItalicButtonToggled(object sender, ToggledEventArgs args)
+        {
+            if (args.Value)
+            {
+                WelcomeLabel.FontAttributes |= FontAttributes.Italic;
+            }
+            else
+            {
+                WelcomeLabel.FontAttributes &= ~FontAttributes.Italic;
+            }
+        }
+    }
+}
